79260809da35c6a7b46b1b93b10749952199b68d
Paul <72827473+pnguyen6910@users.noreply.github.com>
2021-04-14 01:32:42 -0500

    app running

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+const Header = () => {
+    return (
+        <div className="jumbotron text-center">
+            <div className="container text-light">
+                <h1>“The Spear in the Other's heart is the spear in your own: you are he.”  – Diane Duane</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
---
+++ client/src/pages/match.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../components/Header";
+
+const match = () => {
+    return (
+        <div>
+            <Header>
+                <h1>404 Page Not Found</h1>
+                <h1>
+                    <span role="img" aria-label="Face With Rolling Eyes Emoji">
+                    </span>
+                </h1>
+            </Header>
+        </div>
+    );
+}
+
+export default match;
